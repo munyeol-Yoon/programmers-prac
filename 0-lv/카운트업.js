20ac3af760e6fb37dd7c4f4fb17d8afe084bcc93
muyeon <gmakin36@gmail.com>
2024-03-30 08:27:17 +0900

    0Lv: 카운트업

---
+++ 0-lv/카운트업.js
@@ -0,0 +1,11 @@
+function solution(start_num, end_num) {
+  let answer = [];
+
+  for (let i = start_num; i <= end_num; i++) {
+    answer.push(i);
+  }
+
+  return answer;
+}
+
+console.log(solution(3, 10));
