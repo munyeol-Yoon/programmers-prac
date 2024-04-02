ade9fd682f084fe93d175ce84bd7ffc251ec9cde
muyeon <gmakin36@gmail.com>
2024-04-02 12:58:29 +0900

    0Lv: 배열 만들기1

---
+++ 0-lv/배열 만들기1.js
@@ -0,0 +1,13 @@
+function solution(n, k) {
+  let answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % k === 0) {
+      answer.push(i);
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(10, 3));
