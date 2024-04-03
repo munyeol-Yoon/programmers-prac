b6ce120f170de141a998148ddb5f81a3287135e9
muyeon <gmakin36@gmail.com>
2024-04-03 09:07:46 +0900

    0Lv: 카운트 다운

---
+++ 0-lv/카운트 다운.js
@@ -0,0 +1,11 @@
+function solution(start, end_num) {
+  let answer = [];
+
+  for (let i = start; i >= end_num; i--) {
+    answer.push(i);
+  }
+
+  return answer;
+}
+
+console.log(solution(10, 3));
