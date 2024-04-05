a7f7ec79219c1be2e7ec4eb3660d95fb9948c55c
muyeon <gmakin36@gmail.com>
2024-04-05 10:11:43 +0900

    0Lv: rny_string

---
+++ 0-lv/rny_string.js
@@ -0,0 +1,8 @@
+function solution(rny_string) {
+  return rny_string.replaceAll("m", "rn");
+}
+
+console.log(solution("masterpiece"));
+console.log(solution("programmers"));
+console.log(solution("jerry"));
+console.log(solution("burn"));
